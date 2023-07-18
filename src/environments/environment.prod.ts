d43e569f60bf8a0d53dd08759db6468f489abd91
jhoncee <57083899+jhoncee@users.noreply.github.com>
2023-07-18 15:26:40 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiProtocol: "https",
-  apiUrl: "157.230.37.157:3000",
-  apiPort: "3000",
-  apiVersion: "v1",
-
+  apiProtocol: 'http',
+  apiUrl: '146.190.145.208:3005',
+  backendUrl: 'http://localhost:3000',
+  apiPort: '3000',
+  apiVersion: 'v1',
   apkUrl: "https://download1323.mediafire.com/fkani4mkgqhg/vq9najyanqhbr1c/EERS-Responder_v2.3.apk"
 };
