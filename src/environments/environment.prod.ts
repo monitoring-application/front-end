2efc7ac3a24d361c472eac8e7df339c71484e68a
John Stephen Entia <juanmerbbss08@gmail.com>
2023-07-19 12:45:17 +0800

    Fix configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: true,
   apiProtocol: 'http',
   apiUrl: '146.190.145.208:3005',
-  backendUrl: 'http://localhost:3000',
+  backendUrl: 'http://146.190.145.208:3005',
   apiPort: '3000',
   apiVersion: 'v1',
-  apkUrl: "https://download1323.mediafire.com/fkani4mkgqhg/vq9najyanqhbr1c/EERS-Responder_v2.3.apk"
+  apkUrl:
+    'https://download1323.mediafire.com/fkani4mkgqhg/vq9najyanqhbr1c/EERS-Responder_v2.3.apk',
 };
